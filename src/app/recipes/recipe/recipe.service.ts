cb8c27b3ea52ab0ce62209cfff7bda1ebce90150
Gagan Tut <gstut@live.com>
2017-09-20 14:17:27 -0700

    added ingrediants to recipe model and adjusted rest of app accordingly

--- src/app/recipes/recipe/recipe.service.ts
+++ src/app/recipes/recipe/recipe.service.ts
@@ -1,10 +1,21 @@
 import { Injectable, EventEmitter } from '@angular/core';
 
 import {Recipe} from '../recipe.model';
+import { Ingrediant } from '../../shared/ingrediant.model';
 
 @Injectable()
 export class RecipeService {
-	private recipes: Recipe[] = [];
+	private recipes: Recipe[] = [
+		new Recipe(
+			'First Service',
+			'This is the first test recipe',
+			'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+			[
+				new Ingrediant('Meat', 1),
+				new Ingrediant('Fries', 12)
+			]
+		)
+	];
 
 	public recipeSelected = new EventEmitter<Recipe>();
 
