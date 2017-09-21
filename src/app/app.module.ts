5ca63b9262c125970a17bb03512ec7ac08055bb1
Gagan Tut <gstut@live.com>
2017-09-20 18:06:06 -0700

    Added style to active routes for recipe list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 const appRoutes: Routes = [
 	{path: '', redirectTo: '/recipes', pathMatch: 'full'},
@@ -34,7 +35,8 @@ const appRoutes: Routes = [
     ShoppingListComponent,
     ShoppingEditComponent,
 		DropdownDirective,
-		RecipeStartComponent
+		RecipeStartComponent,
+		RecipeEditComponent
   ],
   imports: [
 		BrowserModule,
