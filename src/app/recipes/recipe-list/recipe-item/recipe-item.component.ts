74495f499e355e628ccdca4cd1ed09fbdb724ff3
Gagan Tut <gstut@live.com>
2017-09-20 17:57:29 -0700

    Implemented child routing to go to each specific recipe

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
-
 import {Recipe} from '../../recipe.model';
-import {RecipeService} from '../../recipe/recipe.service'
 
 @Component({
   selector: 'app-recipe-item',
@@ -11,15 +9,8 @@ import {RecipeService} from '../../recipe/recipe.service'
 })
 export class RecipeItemComponent implements OnInit {
 	@Input() recipe: Recipe;
-	@Output()	recipeSelected = new EventEmitter<void>();
-
-  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
-	}
-	
-	onSelected() {
-		this.recipeService.recipeSelected.emit(this.recipe);
 	}
 
 }
--- src/app/recipes/recipe/recipe.service.ts
+++ src/app/recipes/recipe/recipe.service.ts
@@ -28,6 +28,10 @@ export class RecipeService {
 		return this.recipes.slice()
 	}
 
+	getRecipe(index: number) {
+		return this.recipes[index];
+	}
+
 	addIngToList(ingrediants: Ingrediant[]) {
 		this.shoppingService.addIngrediants(ingrediants);
 	}
