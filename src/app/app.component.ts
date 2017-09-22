502e66e5aa55255692a9d12f687f9bb43f6b7716
Gagan Tut <gstut@live.com>
2017-09-22 11:35:36 -0700

    Initialized firebase in app for authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+import { fbKey } from './KEYS';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 	loadedFeature = 'recipe';
 
 	onNavigate(feature: string) {
 		this.loadedFeature = feature;
+	}
+
+	ngOnInit() {
+		firebase.initializeApp({
+			apiKey: fbKey,
+			authDomain: "ngularprac.firebaseapp.com",
+		})
 	}
 
 }
